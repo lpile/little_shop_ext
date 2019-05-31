a8babf19b2233dda3989bd4ac6313f9f7091ec30
lpile <logan.pile@gmail.com>
2019-05-31 04:34:33 -0600

    Add #new and #create action

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,18 @@
 class LocationsController < ApplicationController
   def new
+    @location = Location.new
+    @user = User.find(params[:user_id])
+  end
 
+  def create
+    user = User.find(params[:user_id])
+    location = user.locations.create(location_params)
+    redirect_to profile_path
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:nickname, :address, :city, :state, :zip)
   end
 end
